542c70f7334ebf0c9b34b5cdad30adc1926bd35b
Matti Suominen <matsuominen@gmail.com>
2015-11-04 19:31:03 +0200

    Edited distance_meter and also updated usart driver init

--- avr/src/distance_meter.c
+++ avr/src/distance_meter.c
@@ -3,21 +3,27 @@
  *
  * Created: 7.10.2015 13:45:12
  *  Author: e1100983
+ * 
+ * 1 tick  125ns = 0.125us
  */
 #include <avr/io.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
 #include "include/USART_driver.h"
 
-#define ECHO  PE7
-#define TRIG  (1<<PE6)
+#define ECHO PD4
+#define TRIG (1<<PE6)
+
+#define SCALE 0.125
 #define CM    58
 
 static uint16_t tim = 0;
 static uint8_t flag = 0;
 static uint8_t _ovf = 0;
 static uint8_t c_echo = 0; // echo complete
+uint8_t meas;
 
-//void initINT();
+void initINT();
 void initTIM();
 
 void trigger()
@@ -25,26 +31,24 @@ void trigger()
   uint32_t  trig;
   uint8_t   loop;
   PORTE |= TRIG;
-//  for(loop = 0; loop < 27; loop++)
-//  {
-    for(trig = 8; trig > 0; trig--)
-    {}
-//  }
+  _delay_us(10);
   PORTE &= ~TRIG;
 }
 
 
 ISR(TIMER1_CAPT_vect)
 {
-  if(((TCCR1B >> ICES1) & 1) == 1)
+  if(!flag)
   {
     TCNT1 = 0;
     TCCR1B &= ~(1 << ICES1);
+    flag = 1;
   }
   else
   {
     tim = ICR1;
     c_echo = 1;
+    flag = 0;
     TCCR1B |= (1 << ICES1);
   }
 }
@@ -57,29 +61,35 @@ ISR(TIMER1_OVF_vect)
 
 int main(void)
 {
-  uint32_t dist;
-
-  DDRE &= ~(1 << ECHO);
+  DDRD &= ~(1 << ECHO);
   DDRE |= TRIG;
+  
 
-  initTIM();
   USART_Init();
-
+  initTIM();
+  
   sei();
+  
   trigger();
 
   while(1)
   {
-    if(c_echo)
+    if(c_echo == 1)
     {
-      dist = tim/CM;
+      
       USART_Transmit(USART_MODBUS, (tim >> 8));
       USART_Transmit(USART_MODBUS, tim);
+      
+      meas = (uint8_t)(((double)tim)/CM);
+      USART_Transmit(USART_MODBUS, 0xFF);
 
+      USART_Transmit(USART_MODBUS, (meas));
+      
+      _delay_ms(3000);
       c_echo = 0;
-      trigger();
+      //trigger();
     }
-
+    _delay_ms(100);
   }
   
   return 0;
